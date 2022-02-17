9e4b27f506db9d8001da54d912bf59c4183971b1
PatKiese <kiesepatric@gmail.com>
2022-02-17 20:55:52 +0100

    Implemented next steps towards OOP.

--- main.py
+++ main.py
@@ -2,18 +2,23 @@
 
 # Press Umschalt+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import load_scryfall_to_sqldb
-import create_cleaned_pricing_table
-import util
+from load_scryfall_to_sqldb import DbLoader
+from create_cleaned_pricing_table import DataTransformer
+from util import ConfigManager
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # Create the config manager
     cfg_mgr = ConfigManager(credentials_file_path="./input/credentials.json" , run_id_file_path="./input/runID.json")
-    # load run id from file
-    current_run_id = cfg_mgr.run_id
-    # execute code in order
-    load_scryfall_to_sqldb.load_to_sqldb(current_run_id)
-    create_cleaned_pricing_table.create_pricing_table(current_run_id)
+    db_loader = DbLoader(run_id=cfg_mgr.run_id ,credentials=cfg_mgr.credentials)
+    d_transformer = DataTransformer(run_id=cfg_mgr.run_id)
+    # Fetch data and load to DataFrame
+    df = db_loader.fetch_data()
+    df = d_transformer.prep_data(df)
+    db_loader.load_data_to_db(df, table_name='bulk_data')
+    # Create cleaned pricing table
+    df = db_loader.fetch_table_from_db(run_id=cfg_mgr.run_id)
+    df_prices = d_transformer.create_pricing_table(df)
+    db_loader.load_data_to_db(df_prices, table_name='pricing_table')
     # increment run id by one and save it to file
-    cfg_mgr.save_run_id(file_path="./input/runID.json" , run_id=(current_run_id+1))
+    cfg_mgr.save_run_id(file_path="./input/runID.json" , run_id=(cfg_mgr.run_id + 1))
